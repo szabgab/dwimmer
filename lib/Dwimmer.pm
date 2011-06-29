0745b1dffa7c6fc0591e93feb51fa4a801135b0f
Gabor Szabo <szabgab@gmail.com>
2011-06-29 13:37:56 +0300

    create the possible simplest blog possible

--- lib/Dwimmer.pm
+++ lib/Dwimmer.pm
@@ -1,10 +1,34 @@
 package Dwimmer;
 use Dancer ':syntax';
 
-our $VERSION = '0.1';
+use File::Slurp qw(read_file write_file);
+
+our $VERSION = '0.01';
 
 get '/' => sub {
-    template 'index';
+    my $filename = config->{dwimmer}{json};
+	my $json = -e $filename ? read_file $filename : '{}';
+	my $data = from_json $json;
+    template 'index', {data => $data};
+};
+
+
+get '/page' => sub {
+    template 'page';
+};
+
+post '/page' =>  sub {
+    my $filename = config->{dwimmer}{json};
+	my $json = -e $filename ? read_file $filename : '{}';
+	my $data = from_json $json;
+    my $now   = time;
+	$data->{$now} = {
+		title => params->{title},
+		text  => params->{text},
+	};
+
+	write_file $filename, to_json($data);
+	redirect '/';
 };
 
 true;
