c14934ffb08521e8fb755b65703ee95134bea6fd
Gabor Szabo <szabgab@gmail.com>
2011-07-24 17:13:10 +0300

    add schema and database creation script

---
+++ script/dwimmer_setup.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use DBIx::RunSQL;
+use Digest::SHA qw(sha1_base64);
+use Email::Valid;
+use File::Spec;
+use Getopt::Long qw(GetOptions);
+use String::Random;
+use Pod::Usage  qw(pod2usage);
+use YAML;
+
+my %opt;
+GetOptions(\%opt,
+    'email=s',
+    'password=s',
+);
+usage() if not $opt{email};
+die 'Invalid e-mail' if not Email::Valid->address($opt{email});
+usage() if not $opt{password};
+die 'Password needs to be 6 characters' if length $opt{password} < 6;
+
+
+my $config = YAML::LoadFile('config.yml');
+my $dbfile = $config->{dwimmer}{dbfile};
+my $sql = File::Spec->catfile('share', 'schema', 'dwimmer.sql');
+
+die "Database file '$dbfile' already exists\n" if -e $dbfile;
+
+DBIx::RunSQL->create(
+    dsn => "dbi:SQLite:dbname=$dbfile",
+    sql => $sql,
+    verbose => 0,
+);
+
+my $dbh = DBI->connect("dbi:SQLite:dbname=$dbfile");
+my $time = time;
+my $validation_key = String::Random->new->randregex('[a-zA-Z0-9]{10}') . $time . String::Random->new->randregex('[a-zA-Z0-9]{10}');
+$dbh->do('INSERT INTO user (name, sha1, email, validation_key, approved) VALUES(?, ?, ?, ?, ?)', 
+    {}, 
+    'admin', sha1_base64($opt{password}), $opt{email}, $validation_key, 1);
+
+print <<"END_MSG";
+Database created.
+
+You can now launch the application and visit the web site
+END_MSG
+
+exit;
+
+
+sub usage {
+    pod2usage();
+}
+
+=head1 SYNOPSIS
+
+REQUIRED PARAMETERS:
+
+   --email email        of administrator
+
+   --password PASSWORD  of administrator
+
+=cut
+
---
+++ share/schema/dwimmer.sql
@@ -0,0 +1,39 @@
+CREATE TABLE user (
+    id              INTEGER PRIMARY KEY,
+    name            VARCHAR(30) UNIQUE NOT NULL,
+    sha1            VARCHAR(255),
+    email           VARCHAR(100) UNIQUE NOT NULL,
+    fname           VARCHAR(100),
+    lname           VARCHAR(100),
+    country         VARCHAR(100),
+    state           VARCHAR(100),
+    validation_key  VARCHAR(255),
+    approved        BOOL DEFAULT 0,
+    register_ts     INTEGER DEFAUL NOW
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_user_email ON user (email);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_user_name  ON user (name);
+
+CREATE TABLE site (
+    id            INTEGER PRIMARY KEY,
+    name          VARCHAR(100) UNIQUE NOT NULL,
+    owner         INTEGER NOT NULL,
+    creation_ts   INTEGER NOT NULL DEFAULT NOW ,
+    FOREIGN KEY (owner) REFERENCES user(id)
+);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_site_name ON site (name);
+
+CREATE TABLE page (
+    id          INTEGER PRIMARY KEY,
+    siteid      INTEGER,
+    title       VARCHAR(255),
+    body        BLOB,
+    description VARCHAR(255),
+    abstract    BLOB,
+    filename    VARCHAR(255),
+    timestamp   INTEGER,
+    author      INTEGER,
+    FOREIGN KEY (siteid) REFERENCES site(id),
+    FOREIGN KEY (author) REFERENCES user(id)
+);
