906723fd08c6c5eb57284c008bfa45685f10becf
Gabor Szabo <szabgab@gmail.com>
2011-07-25 15:50:43 +0300

    registration

--- lib/Dwimmer.pm
+++ lib/Dwimmer.pm
@@ -2,7 +2,9 @@ package Dwimmer;
 use Dancer ':syntax';
 
 our $VERSION = '0.01';
+
 use Data::Dumper;
+use String::Random;
 
 use Dwimmer::DB;
 use Dwimmer::Tools qw(sha1_base64);
@@ -93,6 +95,79 @@ foreach my $page (@error_pages) {
         template 'invalid_login';
     };
 }
+
+get '/register' => sub {
+    template 'register';
+};
+
+post '/register' => sub {
+    # validate
+    my $uname  = params->{uname}    || '';
+    my $email  = lc params->{email} || '';
+    my $pw1    = params->{pw1}      || '';
+    my $pw2    = params->{pw2}      || '';
+    my $tos    = params->{tos}      || '';
+
+    my $db = _get_db();
+    $uname =~ s/^\s+|\s+$//g;
+    if (length $uname < 2 or $uname =~ /[^\w.-]/) {
+        return 'Invalid username';
+    }
+    my $user = $db->resultset('User')->find( {name => $uname});
+    if ($user) {
+        return 'This username is already taken';
+    }
+    $user = $db->resultset('User')->find( {email => $email});
+    if ($user) {
+        return 'This email was already used. Would you like to reset your password?';
+    }
+    if (length $pw1 < 5) {
+        return 'Password is too short. It needs at least 5 characters';
+    }
+    if ($pw1 ne $pw2) {
+        return 'Passwords did not match';
+    }
+    if ($tos ne 'on') {
+        return 'Sorry we cannot register you if you dont agree to our Terms of Service';
+    };
+
+    # insert new user
+    my $time = time;
+    my $validation_key = String::Random->new->randregex('[a-zA-Z0-9]{10}') . $time . String::Random->new->randregex('[a-zA-Z0-9]{10}');
+    $user = $db->resultset('User')->create({
+        name  => $uname,
+        email => $email,
+        sha1  => sha1_base64($pw1),
+        validation_key => $validation_key,
+    });
+    
+    my $template = read_file(path(config->{appdir}, 'view', 'register_verify_mail'));
+    use Template;
+    if ($user) {
+        my $url = 'http://' . request->host . "/finish_registration?uname=$uname&code=$validation_key";
+        #my $template = read_file 
+        my $message = ''; # template 'register_verify_mail', { url => $url };
+        my $msg = MIME::Lite->new(
+            From    => 'gabor@szabgab.com',
+            To      => $email,
+            Subject => 'Verify your registration to Dwimmer!',
+            Data    => $message,
+        );
+        $msg->send;
+    }
+    redirect '/register_done';
+};
+
+get '/manage' => sub {
+    template 'manage';
+};
+
+
+get '/edit_this_page' => sub {
+    return 'edit this page';
+};
+
+
 
 
 ###### helper methods
