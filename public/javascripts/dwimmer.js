88ee3a936dd17d9183f4744b5c07e68c9bac3af6
Gabor Szabo <gabor@szabgab.com>
2011-08-07 22:38:21 +0300

    cleanup

--- public/javascripts/dwimmer.js
+++ public/javascripts/dwimmer.js
@@ -9,12 +9,6 @@ var original_content; // to make editor cancellation quick
    $('#manage-bar').hide();
    $('#manage-bar > div').hide();
    
-//   $('#manage-bar div').hide();
-//   $('#manage-bar').show();
-//   $('#manage-display').html('editor_body');
-//   $('#manage-display').show();
-//   $('#manage-close').show();
-
    $.getJSON('/_dwimmer/session.json', function(resp) {
        if (resp["logged_in"] == 1) {
            $('#logged_in_bar').show();
@@ -32,14 +26,11 @@ var original_content; // to make editor cancellation quick
        original_content = $('#content').html();
        var url = '/_dwimmer/page.json?filename=' + $(location).attr('pathname');
        $.getJSON(url, function(resp) {
-//            $('#content').hide();
             $('#admin-editor').show();
-            //$('#admin-preview').show();
             $('#filename').val( resp["page"]["filename"] );
             $('#editor_title').val( resp["page"]["title"] );
             $('#editor_body').val( resp["page"]["body"] );
             $('#editor_body').keyup();    // update preview
-            //$('#editor_body').html(resp);
        });
        return false;
    });
@@ -47,11 +38,9 @@ var original_content; // to make editor cancellation quick
     $('#cancel').click(function(){
         $('#editor_title').val('');
         $('#editor_body').val('');
-        //$('#content').show();
         $('#content').html(original_content);
         original_content = '';
         $('#admin-editor').hide();
-        //$('#admin-preview').hide();
         $(".close_manage_bar").click();
         return false;
     });
@@ -127,13 +116,11 @@ var original_content; // to make editor cancellation quick
     });
 
     $(".add_user").click(function(){
-        //alert("TODO: add user");
         manage_bar();
         $('#manage-add-user').show();
         return false;
     });
     $("#add-user-form").submit(function() {
-     // var url = $(this).attr('action') + '?' + $(this).serialize();
         var url = "/_dwimmer/add_user.json";
         $.post(url, $(this).serialize(), function(resp) {
             if (resp["success"] == 1) {
@@ -148,7 +135,6 @@ var original_content; // to make editor cancellation quick
 
     $(".add_page").click(function(){
         alert("TODO: add page");
-        // resize the managemen bar (or add another bar?)
         return false;
     });
 
@@ -169,11 +155,9 @@ var original_content; // to make editor cancellation quick
 
 
     $("form.login").submit(function() {
-     // var url = $(this).attr('action') + '?' + $(this).serialize();
         var url = "/_dwimmer/login.json";
         $.post(url, $(this).serialize(), function(resp) {
             if (resp["success"] == 1) {
-                // TODO update the username, userid in the browser, should come with the success response
                 $('#guest_bar').hide();
                 $('#logged_in_bar').show();
                 $("#logged-in").html(resp["username"]);
@@ -241,48 +225,4 @@ function markup(text) {
     html = html.replace(/\[([\w\/\-]+)\]/g, '<a href="$1">$1</a>');
     return html;
 }
-
-function send_query() {
-            var query = $('#query').val();
-            //alert($('#what').val());
-            var what = $('#what').val();
-            $('#result').html('Searching ...');
-            $.get('/q/' + query + '/' + what, function(resp) {
-                    $('#content').hide();
-                    if (resp["error"]) {
-                       alert(resp["error"]);
-                    } else {
-//alert(resp);
-//                     $('#result').html('ok');
-                       var html = '';
-                       var data = resp["data"];
-                       for (var i=0; i<data.length; i++) {
-                           // distribution
-                           if (data[i]["distribution"] == '1') {
-                                html += '<div class="author"><a href="/id/' + data[i]["author"]   + '">' + data[i]["author"] + '</a></div>';
-                                html += '<div class="name"><a href="/dist/' + data[i]["name"] + '">' + data[i]["name"]   + '</a></div>';
-                                html += '<div class="version">' + data[i]["version"] + '</div>';
-                           }
-                           // author
-                           if (data[i]["author"] == '1') {
-                                var name = data[i]["asciiname"];
-                                if (data[i]["name"]) {
-                                        name = data[i]["name"];
-                                }
-                                html += '<div class="name"><a href="/id/' + data[i]["pauseid"] + '">' + data[i]["pauseid"] + '(' + name + ')' + '</a></div>';
-                                if (data[i]["homepage"]) {
-                                        html += '<div class="name"><a href="' + data[i]["homepage"] + '">' + data[i]["homepage"]   + '</a></div>';
-                                }
-                           }
-                          
-                           html += '<br>';
-                       }
-                       $('#result').html(html);
-                    }
-                    if (resp["ellapsed_time"]) {
-                        $('#ellapsed_time').html("Ellapsed time: " + resp.ellapsed_time);
-                    }
-            }, 'json');
-            return false;
-};
 
