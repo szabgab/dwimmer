2d8e5bdbae12de257512d53797646bfbdecef912
Gabor Szabo <szabgab@gmail.com>
2011-08-28 07:41:22 +0300

    tidy

--- t/lib/Dwimmer/Test.pm
+++ t/lib/Dwimmer/Test.pm
@@ -15,43 +15,45 @@ my $process;
 
 sub start {
     my ($password) = @_;
-    return if $^O !~ /win32/i; # this test is for windows only now
+    return if $^O !~ /win32/i;    # this test is for windows only now
 
-my $dir = tempdir( CLEANUP => 1 );
-#print STDERR "# $dir\n";
+    my $dir = tempdir( CLEANUP => 1 );
 
-$ENV{DWIMMER_TEST} = 1;
-$ENV{DWIMMER_PORT} = 3001;
-$ENV{DWIMMER_MAIL} = File::Spec->catfile($dir, 'mail.txt');
+    #print STDERR "# $dir\n";
 
-our $admin_mail = 'test@dwimmer.org';
+    $ENV{DWIMMER_TEST} = 1;
+    $ENV{DWIMMER_PORT} = 3001;
+    $ENV{DWIMMER_MAIL} = File::Spec->catfile( $dir, 'mail.txt' );
 
-our @users = (
-	{
-		uname    => 'tester',
-		fname    => 'foo',
-		lname    => 'bar',
-		email    => 'test@dwimmer.org',
-		password => 'dwimmer',
-	},
-);
+    our $admin_mail = 'test@dwimmer.org';
 
-my $root = File::Spec->catdir($dir, 'dwimmer');
-system "$^X -Ilib script/dwimmer_setup.pl --root $root --email $admin_mail --password $password";
+    our @users = (
+        {   uname    => 'tester',
+            fname    => 'foo',
+            lname    => 'bar',
+            email    => 'test@dwimmer.org',
+            password => 'dwimmer',
+        },
+    );
 
+    my $root = File::Spec->catdir( $dir, 'dwimmer' );
+    system
+        "$^X -Ilib script/dwimmer_setup.pl --root $root --email $admin_mail --password $password";
 
-    if ($^O =~ /win32/i) {
+    if ( $^O =~ /win32/i ) {
         require Win32::Process;
+
         #import Win32::Process;
 
-        Win32::Process::Create($process, $^X,
-                            "perl -Ilib -It\\lib $root\\bin\\app.pl",
-                            0,
-                            Win32::Process::NORMAL_PRIORITY_CLASS(),
-                            ".") || die ErrorReport();
-    } else {
+        Win32::Process::Create( $process, $^X,
+            "perl -Ilib -It\\lib $root\\bin\\app.pl",
+            0, Win32::Process::NORMAL_PRIORITY_CLASS(), "." )
+            || die ErrorReport();
+    }
+    else {
+
         #warn "Unsupported OS\n";
-	return;
+        return;
     }
 
     return 1;
@@ -59,17 +61,16 @@ system "$^X -Ilib script/dwimmer_setup.pl --root $root --email $admin_mail --pas
 
 sub stop {
     return if not $process;
-    if ($^O =~ /win32/i) {
+    if ( $^O =~ /win32/i ) {
         $process->Kill(0);
-    #} else {
-    #    warn "Unsupported OS\n";
+
+        #} else {
+        #    warn "Unsupported OS\n";
     }
 }
 
 END {
     stop();
 }
-
-
 
 1;
