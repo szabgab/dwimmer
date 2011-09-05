6e8c8496f132f3ba82cf7ff46fa2af89e562f3ce
Gabor Szabo <szabgab@gmail.com>
2011-09-06 00:40:48 +0300

    change the structure of the source file

--- script/feed_collector.pl
+++ script/feed_collector.pl
@@ -18,45 +18,55 @@ if (-e $storage) {
 	#print keys %$data;
 }
 my $sources = from_json scalar read_file $file;
-for my $chapter ( @{ $sources->{chapters} } ) {
-	for my $e ( @{ $chapter->{entries} } ) {
-		if ($e->{feed}) {
-			LOG("Processing $e->{title}");
-			my $feed = XML::Feed->parse(URI->new($e->{feed}));
-			if (not $feed) {
-				LOG("ERROR: " . XML::Feed->errstr);
-				next;
-			}
-			if ($feed->title) {
-				LOG("Title: " . $feed->title);
-			} else {
-				LOG("WARN: no title");
-			}
-			for my $entry ($feed->entries) {
-				#print $entry, "\n";
-				
-				if (not $data->{$entry->link}) {
-					my %current = (
-						link    => $entry->link,
-						title   => ($entry->title || ''),
-						summary => ($entry->summary->body || ''),
-						content => ($entry->content->body || ''),
-					);
-					$data->{$entry->link} = \%current;
-					
-					my $mail = '';
-					$mail .= "$current{title}\n";
-					$mail .= "$current{link}\n\n";
-					$mail .= "$current{summary}\n\n";
-					$mail .= "$current{content}\n\n------------------\n\n";
-					sendmail("Feed: $current{title}", $mail);
-				}
-			}
-		} else {
-			warn "No feed for $e->{title}\n";
+
+for my $e ( @{ $sources->{feeds}{entries} } ) {
+	if ($e->{feed}) {
+		LOG("Processing $e->{title}");
+		my $feed = XML::Feed->parse(URI->new($e->{feed}));
+		if (not $feed) {
+			LOG("ERROR: " . XML::Feed->errstr);
+			next;
 		}
+		if ($feed->title) {
+			LOG("Title: " . $feed->title);
+		} else {
+			LOG("WARN: no title");
+		}
+		for my $entry ($feed->entries) {
+			#print $entry, "\n";
+			
+			if (not $data->{$entry->link}) {
+				my %current = (
+					source  => $e->{feed},
+					link    => $entry->link,
+					author  => ($entry->{author} || ''),
+					id      => ($entry->{id} || ''),
+					issued  => $entry->issued->ymd . ' ' . $entry->issued->hms,
+					title   => ($entry->title || ''),
+					summary => ($entry->summary->body || ''),
+					content => ($entry->content->body || ''),
+					tags    => '', #$entry->tags,
+				);
+				$data->{$entry->link} = \%current;
+				
+				my $mail = '';
+				$mail .= "$current{title}\n";
+				$mail .= "Link: $current{link}\n\n";
+				$mail .= "Source: $current{source}\n\n";
+				$mail .= "Tags: $current{tags}\n\n";
+				$mail .= "Author: $current{author}\n\n";
+				$mail .= "Date: $current{issued}\n\n";
+				$mail .= "Summary: $current{summary}\n\n";
+				$mail .= "Content: $current{content}\n\n";
+				$mail .= "-------------------------------\n\n";
+				sendmail("Feed: $current{title}", $mail);
+			}
+		}
+	} else {
+		warn "No feed for $e->{title}\n";
 	}
 }
+
 #print $mail;
 #exit;
 sub sendmail {
@@ -70,7 +80,7 @@ sub sendmail {
 	$msg->send;
 }
 
-write_file $storage, { binmode => ':utf8' }, to_json $data;
+write_file $storage, { binmode => ':utf8' }, to_json $data, {utf8 => 1, pretty => 1};
 
 sub LOG {
 	print "@_\n";
