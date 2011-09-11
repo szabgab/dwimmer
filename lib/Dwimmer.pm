b48a035cd913ed8c9f02e36daaf33f289b713a9e
Gabor Szabo <szabgab@gmail.com>
2011-09-11 12:37:52 +0300

    add some minimal pods

--- lib/Dwimmer.pm
+++ lib/Dwimmer.pm
@@ -45,3 +45,23 @@ get qr{^/([a-zA-Z0-9]\w*)?$} => \&route_index;
 
 
 true;
+
+=head1 NAME
+
+Dwimmer - A platform to develop things
+
+=head1 COPYRIGHT
+
+(c) 2011 Gabor Szabo
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl 5 itself.
+
+=cut
+
+# Copyright 2011 Gabor Szabo
+# LICENSE
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl 5 itself.
