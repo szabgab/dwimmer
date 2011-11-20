f71a9feca43799220e3e9ecd0369ab6562c6ef6e
Gabor Szabo <szabgab@gmail.com>
2011-11-20 12:52:11 +0200

    set version number to 0.23

--- lib/Dwimmer/Client.pm
+++ lib/Dwimmer/Client.pm
@@ -8,7 +8,7 @@ has host => ( is => 'ro', isa => 'Str', required => 1 );
 has mech => ( is => 'rw', isa => 'WWW::Mechanize', default => sub { WWW::Mechanize->new } );
 
 
-our $VERSION = '0.22';
+our $VERSION = '0.23';
 
 # get_user parameters can be    id => 1
 
--- lib/Dwimmer/Tools.pm
+++ lib/Dwimmer/Tools.pm
@@ -9,7 +9,7 @@ use YAML;
 
 use Dwimmer::DB;
 
-our $VERSION = '0.22';
+our $VERSION = '0.23';
 
 our $SCHEMA_VERSION = 1;
 
