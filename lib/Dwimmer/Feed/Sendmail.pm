2907e366088583e06420448411e25874879d776c
Gabor Szabo <szabgab@gmail.com>
2011-12-10 15:19:33 +0200

    add html version to the e-mail

--- lib/Dwimmer/Feed/Sendmail.pm
+++ lib/Dwimmer/Feed/Sendmail.pm
@@ -23,19 +23,21 @@ sub send {
 	my $entries = $self->db->get_queue( 'mail' );
 
 	foreach my $e (@$entries) {
-		my $mail = '';
-		$mail .= "Title: $e->{title}\n";
-		$mail .= "Link: $e->{link}\n\n";
-		$mail .= "Source: $e->{source}\n\n";
-		$mail .= "Tags: $e->{tags}\n\n";
-		$mail .= "Author: $e->{author}\n\n";
-		$mail .= "Date: $e->{issued}\n\n";
-		$mail .= "Summary:\n$e->{summary}\n\n";
-		#$mail .=  Encode::encode('UTF-8', "Content:\n$e->{content}\n\n");
-		#$mail .= "-------------------------------\n\n";
-		$mail .= "Tweet: http://twitter.com/home?status=$e->{title}%20$e->{link}\n";
+		my $text = '';
+		$text .= "Title: $e->{title}\n";
+		$text .= "Link: $e->{link}\n\n";
+		$text .= "Source: $e->{source}\n\n";
+		$text .= "Tags: $e->{tags}\n\n";
+		$text .= "Author: $e->{author}\n\n";
+		$text .= "Date: $e->{issued}\n\n";
+		$text .= "Summary:\n$e->{summary}\n\n";
+		#$text .=  Encode::encode('UTF-8', "Content:\n$e->{content}\n\n");
+		#$text .= "-------------------------------\n\n";
 
-		_sendmail("Perl Feed: $e->{title}", $mail);
+		my $html = '';
+		$html .= qq{<p><a href="http://twitter.com/home?status=$e->{title} $e->{link}">tweet</a></p>};
+
+		_sendmail("Perl Feed: $e->{title}", { text => $text, html => $html } );
 		$self->db->delete_from_queue('mail', $e->{id});
 	}
 
@@ -52,8 +54,27 @@ sub _sendmail {
 		From    => 'dwimmer@dwimmer.com',
 		To      => 'szabgab@gmail.com',
 		Subject => $subject,
-		Data    => $content,
+		Type    => 'multipart/alternative',
 	);
+	my %type = (
+		text => 'text/plain',
+		html => 'text/html',
+	);
+
+	foreach my $t (qw(text html)) {
+		my $att = MIME::Lite->new(
+			Type     => 'text',
+			Data     => $content->{$t},
+			Encoding => 'quoted-printable',
+		);
+		$att->attr("content-type" => "$type{$t}; charset=UTF-8");
+		$att->replace("X-Mailer" => "");
+		$att->attr('mime-version' => '');
+		$att->attr('Content-Disposition' => '');
+
+		$msg->attach($att);
+	}
+
 	$msg->send;
 }
 
